97b01cbb587d5062dba02bcaec32ea03a088d5d6
Muaz Hussain <muaz.self@gmail.com>
2023-04-20 16:59:49 +0600

    Create convertToWave.cpp

---
+++ Arrays/convertToWave.cpp
@@ -0,0 +1,13 @@
+// Problem link: https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr) {
+        for(int i = 1; i < n; i += 2) {
+            swap(arr[i], arr[i - 1]);
+        }  
+    }
+};
