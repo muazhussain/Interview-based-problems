b5a10c5d0a5fe7ac9084ff761cf9e0ca83139664
Muaz Hussain <muaz.self@gmail.com>
2023-04-30 20:34:59 +0600

    Create isAnagram.cpp

---
+++ NeetCode/Arrays & Hashing/isAnagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> count;
+        for(char ch : s) {
+            count[ch]++;
+        }
+        for(char ch : t) {
+            count[ch]--;
+        }
+        bool res = true;
+        for(auto p : count) {
+            res &= (p.second == 0);
+        }
+        return res;
+    }
+};
+
+/*
+	Time complexity: O(n)
+	Space complexity: O(n)
+	
+	Explanation: Count frequency of each character of both strings. If the frequency of each character is equal then it's an anagram otherwise it's not.
+*/
