8980bd7167de8428425b672804be3177385f3b7f
Muaz Hussain <muaz.self@gmail.com>
2023-05-07 01:23:33 +0600

    Update 02. isSequence.cpp

--- LeetCode/02. isSequence.cpp
+++ LeetCode/02. isSequence.cpp
@@ -1,1 +1,23 @@
+// Problem link: https://leetcode.com/problems/is-subsequence/
 
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int lenS = s.size(), lenT = t.size();
+        int i = 0, j = 0;
+        while(i < lenS && j < lenT) {
+            // character matched
+            if(s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        // Returns true if it reaches the end of the subsequence, false otherwise.
+        return i == lenS;
+    }
+};
+
+// Time complexity: O(n)
+// Space complexity: O(1)
